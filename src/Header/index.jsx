d16815f2053e0c10b77eadb134daeda5dfd5bb65
samenezes <61150892+samenezes@users.noreply.github.com>
2023-08-22 10:20:52 -0300

    Add files via upload

---
+++ src/Header/index.jsx
@@ -0,0 +1,14 @@
+import styles from './Header.module.css'
+
+function Header() {
+    return (
+        <header className={styles.header}>
+            <span>Gabriel Mello</span>
+            <nav>
+              <h1>Prova React FrameWork Gabriel Mello15</h1>
+            </nav>
+        </header>
+    )    
+}
+
+export default Header
---
+++ src/jogos.jsx
@@ -0,0 +1,17 @@
+import './jogos.css'
+
+function jogos(){
+
+    var url = 'https://opentdb.com/api.php?amount=30&category=15';     
+
+var uljogos = document.querySelector('#tabjogos');
+var category = document.querySelector('#countcategory')
+var difficulty = document.querySelector('#countdifficulty')
+var question = document.querySelector('#countquestion')
+
+
+var category = [];
+var difficulty = [];
+var question = [];
+
+}
